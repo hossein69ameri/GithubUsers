f9b3cafb272aced57ce3f1569b68bc37b5da7430
Hossein <hossein.arabameri69@gmail.com>
2023-02-05 15:46:12 +0330

    Design Detail screen

---
+++ app/src/main/java/com/example/githubusers/ui/detail/DetailFragment.kt
@@ -0,0 +1,24 @@
+package com.example.githubusers.ui.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.githubusers.R
+import com.example.githubusers.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+    private lateinit var binding: FragmentDetailBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentDetailBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
