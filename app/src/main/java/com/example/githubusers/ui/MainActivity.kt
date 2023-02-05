55f931ff5dd7807159838d89d07191887fa2644e
Hossein <hossein.arabameri69@gmail.com>
2023-02-05 11:11:47 +0330

    Import Assets

--- app/src/main/java/com/example/githubusers/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubusers/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.githubusers
+package com.example.githubusers.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.githubusers.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
